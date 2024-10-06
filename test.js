e4addc47ed9cd96ce352a878b70c7bd607b4da69
Kunal Tiwari <kunaltiwari5596@gmail.com>
2024-10-06 08:01:41 +0530

    test case fix

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const greet = require('./index');
 const assert = require('assert');
 
-assert.strictEqual(greet('World'), 'Hello World!');
+assert.strictEqual(greet('World'), 'Hello World');
 console.log('All tests passed!');
